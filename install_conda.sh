2dc8179116daad9aa4c83a0e2d67a40f253664d1
ilhamadi8 <156057693+ilhamadi8@users.noreply.github.com>
2024-08-04 10:57:10 +0800

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -1,5 +1,7 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sha256sum Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+source $HOME/miniconda3/etc/profile.d/conda.sh
+echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 conda activate
